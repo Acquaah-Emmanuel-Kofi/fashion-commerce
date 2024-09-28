23e07690cc565e50254b8b3ba8d4a46ab5c57dce
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-28 06:59:37 +0000

    feat: install react icons package

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { FaBeer } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <h3>
+        {" "}
+        Lets go for a <FaBeer />?{" "}
+      </h3>
     </div>
   );
 }
