9782dc8bf1dc60ae5ab71991f7dad887328a1353
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-28 12:28:39 +0000

    feat: render dummy products on page

--- app/shared/components/NewProducts.tsx
+++ app/shared/components/NewProducts.tsx
@@ -47,6 +47,58 @@ const NewProducts = () => {
         </h1>
       </div>
 
+      <main>
+        <div className="flex justify-end items-center mb-5 px-6">
+          <button type="button" className="flex">
+            See all
+          </button>
+        </div>
+
+        <div className="overflow-auto scrollbar-none w-full pl-6">
+          <div className="flex space-x-4">
+            {products.map((product) => (
+              <div
+                key={product.id}
+                className="w-[170px] lg:w-[366px]  bg-white overflow-hidden"
+              >
+                {/* Product Image */}
+                <div className="relative">
+                  <Image
+                    src={product.img}
+                    alt={product.title}
+                    className="w-full h-[175px] lg:h-[376px] object-cover"
+                  />
+                  <button
+                    type="button"
+                    className="flex justify-between bg-[#DCDCDC70] text-[#0C0C0C] items-center p-2.5 absolute bottom-1 left-2/4 -translate-x-2/4"
+                  >
+                    <HiPlus />
+                  </button>
+                </div>
+
+                {/* Product Details */}
+                <div className="p-2">
+                  <h1 className="text-sm lg:text-lg font-bold">
+                    {product.title}
+                  </h1>
+                  <div className="flex items-center justify-between">
+                    <div className="text-gray-600 text-xs lg:text-sm">
+                      {product.description}
+                    </div>
+                    <label
+                      htmlFor="price"
+                      className="block mt-2 text-black font-semibold lg:text-lg"
+                    >
+                      ${product.price}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+
     </section>
   );
 };
