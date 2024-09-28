a35d757b999e3fdab716e6772b3b021474bd4dc1
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-28 10:46:38 +0000

    refactor: update page with custom layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Navbar from "./shared/components/Navbar";
+import Layout from "./shared/components/Layout";
+import ProductSlider from "./shared/components/ProductSlider";
+import Sidebar from "./shared/components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-    </div>
+    <Layout showFooter={true}>
+      <div className="flex flex-col lg:flex-row">
+        <Sidebar />
+        <div className="w-full lg:w-3/4 p-4 lg:p-6 hidden lg:block">
+          <ProductSlider />
+        </div>
+      </div>
+    </Layout>
   );
 }
