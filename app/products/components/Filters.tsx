f624d92fbe9227c42c87d6771b1b7beeb5019d69
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-28 22:24:02 +0000

    refactor: remove filter label from filters component

--- app/products/components/Filters.tsx
+++ app/products/components/Filters.tsx
@@ -19,29 +19,24 @@ const Filters: React.FC<IFiltersProps> = ({ filters }) => {
 
   return (
     <Fragment>
-      <div>
-        <h2 className="font-semibold text-lg mb-6">Filters</h2>
-        <div>
-          <label htmlFor="size" className="font-semibold">
-            Size
-          </label>
-          <div className="flex space-x-1 border-b border-dashed border-gray-200 pb-5 mb-2 mt-3">
-            {sizeOptions.map((size) => (
-              <button
-                key={size}
-                type="button"
-                className={`flex justify-center items-center w-11 h-11 border border-[#D9D9D9] p-2.5 hover:bg-[#D9D9D9] ${
-                  selectedSize === size
-                    ? "bg-black text-white"
-                    : "bg-white text-black"
-                }`}
-                onClick={() => handleSizeClick(size)}
-              >
-                {size}
-              </button>
-            ))}
-          </div>
-        </div>
+      <label htmlFor="size" className="font-semibold">
+        Size
+      </label>
+      <div className="flex space-x-1 border-b border-dashed border-gray-200 pb-5 mb-2 mt-3">
+        {sizeOptions.map((size) => (
+          <button
+            key={size}
+            type="button"
+            className={`flex justify-center items-center w-11 h-11 border border-[#D9D9D9] p-2.5 hover:bg-[#D9D9D9] ${
+              selectedSize === size
+                ? "bg-black text-white"
+                : "bg-white text-black"
+            }`}
+            onClick={() => handleSizeClick(size)}
+          >
+            {size}
+          </button>
+        ))}
       </div>
       <div className="space-y-2">
         {filters.map((filter) => (
