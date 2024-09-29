bed61ee5f821c193551ae3fed9c65c2721ac14ba
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-29 13:46:17 +0000

    refactor: use size filters component in filters on product sidebar

--- app/products/components/Filters.tsx
+++ app/products/components/Filters.tsx
@@ -2,9 +2,9 @@ import { Fragment, useState } from "react";
 import { IoIosArrowForward, IoIosArrowUp } from "react-icons/io";
 import { sizeOptions } from "@/app/shared/helpers/constants.helper";
 import { IFiltersProps } from "../interfaces/filters.interface";
+import SizeFilter from "@/app/shared/components/SizeFilter";
 
 const Filters: React.FC<IFiltersProps> = ({ filters }) => {
-  const [selectedSize, setSelectedSize] = useState<string | null>(null);
   const [openFilters, setOpenFilters] = useState<{ [key: string]: boolean }>(
     {}
   );
@@ -13,30 +13,10 @@ const Filters: React.FC<IFiltersProps> = ({ filters }) => {
     setOpenFilters((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
-  const handleSizeClick = (size: string) => {
-    setSelectedSize(size);
-  };
-
   return (
     <Fragment>
-      <label htmlFor="size" className="font-semibold">
-        Size
-      </label>
-      <div className="flex space-x-1 border-b border-dashed border-gray-200 pb-5 mb-2 mt-3">
-        {sizeOptions.map((size) => (
-          <button
-            key={size}
-            type="button"
-            className={`flex justify-center items-center w-11 h-11 border border-[#D9D9D9] p-2.5 hover:bg-[#D9D9D9] ${
-              selectedSize === size
-                ? "bg-black text-white"
-                : "bg-white text-black"
-            }`}
-            onClick={() => handleSizeClick(size)}
-          >
-            {size}
-          </button>
-        ))}
+      <div className="border-b border-dashed border-gray-200 pb-5 mb-2 mt-3">
+        <SizeFilter />
       </div>
       <div className="space-y-2">
         {filters.map((filter) => (
