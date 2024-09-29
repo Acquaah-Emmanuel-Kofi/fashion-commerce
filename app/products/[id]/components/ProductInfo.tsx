e99aa2cac5ab0875e973809f863caec08e947235
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-29 13:47:08 +0000

    feat: create product info component

---
+++ app/products/[id]/components/ProductInfo.tsx
@@ -0,0 +1,28 @@
+import ColorFilter from "@/app/shared/components/ColorFilter";
+import SizeFilter from "@/app/shared/components/SizeFilter";
+
+const ProductInfo = () => {
+  return (
+    <div className="">
+      <h1 className="text-sm font-bold">ABSTRACT PRINT SHIRT</h1>
+      <p className="text-sm font-bold">$99</p>
+      <p className="text-gray-600 text-xs">MRP incl. of all taxes</p>
+      <p className="text-xs font-semibold mt-9">
+        Relaxed-fit shirt. Camp collar and short sleeves. Button-up front.
+      </p>
+
+      <div className="space-y-5 mt-16">
+        <div>
+          <ColorFilter />
+        </div>
+        <div>
+          <SizeFilter />
+        </div>
+      </div>
+
+      <p className="text-slate-400 text-xs my-4">FIND YOUR SIZE | MEASUREMENT GUIDE</p>
+    </div>
+  );
+};
+
+export default ProductInfo;
