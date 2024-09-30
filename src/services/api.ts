90cabbcd32451e977f4101f9c1ebe87d5fa2e32a
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-30 12:27:25 +0000

    feat: implement a function to fetch data from an api by passing the endpoint as a param to the function

---
+++ src/services/api.ts
@@ -0,0 +1,7 @@
+export const fetchDataFromApi = async (endpoint: string) => {
+  const response = await fetch(endpoint);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
+};
