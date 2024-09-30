44fd6c0a66cc3d46f83d425f8521a5fc02938932
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-30 00:05:05 +0000

    refactor: update filterproducts component with dummy data

--- app/products/components/FilterProducts.tsx
+++ app/products/components/FilterProducts.tsx
@@ -1,37 +1,5 @@
-import pro1 from "../../../assets/images/product_1.png";
-import pro2 from "../../../assets/images/product_2.png";
 import ProductCard from "@/app/shared/components/ProductCard";
-
-const products = [
-  {
-    id: 1,
-    img: pro1,
-    title: "Cotton T Shirt",
-    description: "Full Sleeve Zipper",
-    price: 199,
-  },
-  {
-    id: 2,
-    img: pro2,
-    title: "Cotton T Shirt",
-    description: "Full Sleeve Zipper",
-    price: 199,
-  },
-  {
-    id: 3,
-    img: pro1,
-    title: "Cotton T Shirt",
-    description: "Full Sleeve Zipper",
-    price: 199,
-  },
-  {
-    id: 4,
-    img: pro2,
-    title: "Cotton T Shirt",
-    description: "Full Sleeve Zipper",
-    price: 199,
-  },
-];
+import { dummyProducts } from "@/app/shared/helpers/constants.helper";
 
 const FilterProducts = () => {
   const handleAddToCart = (id: number) => {
@@ -40,7 +8,7 @@ const FilterProducts = () => {
 
   return (
     <div className="grid lg:grid-cols-3 grid-cols-2 gap-4">
-      {products.map((product) => (
+      {dummyProducts.map((product) => (
         <ProductCard
           key={product.id}
           id={product.id}
