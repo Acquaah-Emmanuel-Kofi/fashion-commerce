24b61c54e731b2f5596d6abef187b4342b243f88
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-30 13:55:04 +0000

    refactor: update product card ui

--- src/app/shared/components/ProductCard.tsx
+++ src/app/shared/components/ProductCard.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
-import { IProductProps } from "../interfaces/constants.interface";
 import Link from "next/link";
+import { IProduct } from "@/modules/interfaces/products.interface";
 
-const ProductCard: React.FC<IProductProps> = ({
-  img,
+const ProductCard: React.FC<IProduct> = ({
+  thumbnail,
   title,
   description,
   price,
-  id
+  id,
 }) => {
   return (
     <Link
       href={`/products/${id}`}
-      className="w-full max-h-[250px] lg:max-h-[450px] bg-white overflow-hidden"
+      className="relative w-full max-h-[250px] lg:max-h-[450px] bg-white overflow-hidden border-2 border-[#D9D9D9] cursor-pointer"
     >
-      <div className="relative">
+      <div>
         <Image
-          src={img}
+          src={thumbnail}
           alt={title}
           className="w-full max-h-[200px] lg:max-h-[400px] object-cover"
           width={300}
@@ -24,16 +24,17 @@ const ProductCard: React.FC<IProductProps> = ({
         />
       </div>
 
+      <div className="absolute inset-0 bg-black bg-opacity-0 hover:bg-opacity-10 transition-opacity duration-200 ease-in-out"></div>
+
       <div className="p-2">
-        <h1 className="text-sm lg:text-lg font-bold">{title}</h1>
+        <h1 className="text-gray-600 text-sm lg:text-base">{title}</h1>
         <div className="flex items-center justify-between">
-          <div className="text-gray-600 text-xs lg:text-sm">{description}</div>
-          <label
-            htmlFor="price"
-            className="block mt-2 text-black font-semibold lg:text-lg"
-          >
+          <p className="text-base lg:text-lg font-bold line-clamp-1">
+            {description}
+          </p>
+          <p className="block mt-2 text-black font-semibold lg:text-lg">
             ${price}
-          </label>
+          </p>
         </div>
       </div>
     </Link>
