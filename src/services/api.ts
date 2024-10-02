e6c3b47d0346e342d505a160c90197a270384a3f
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-02 12:50:50 +0000

    refactor: update fetchDataFromApi function

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 export const fetchDataFromApi = async (endpoint: string) => {
-  const response = await fetch(endpoint);
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
+  try {
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
   }
-  return response.json();
 };
