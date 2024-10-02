2a7dbc83caf1618c52bbfb5d7300726792100b3a
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-02 12:58:19 +0000

    refactor: update product card interface

--- src/app/shared/components/ProductCard.tsx
+++ src/app/shared/components/ProductCard.tsx
@@ -2,44 +2,51 @@ import Image from "next/image";
 import Link from "next/link";
 import { IProduct } from "@/modules/interfaces/products.interface";
 
-const ProductCard: React.FC<IProduct> = ({
+interface ProductCardProps extends IProduct {
+  showProductDetails?: boolean;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({
   thumbnail,
-  title,
+  name,
   description,
   price,
   id,
+  showProductDetails = true,
 }) => {
   return (
     <Link
       href={`/products/${id}`}
       className="relative w-full max-h-[250px] lg:max-h-[450px] bg-white overflow-hidden cursor-pointer"
     >
-      <div>
+      {thumbnail && (
         <Image
           src={thumbnail}
-          alt={title}
+          alt={name}
           className="w-full max-h-[200px] lg:max-h-[400px] object-cover border-2 border-[#D9D9D9]"
           width={300}
           height={376}
           priority
         />
-      </div>
+      )}
 
       <div className="absolute inset-0 bg-black bg-opacity-0 hover:bg-opacity-10 transition-opacity duration-200 ease-in-out"></div>
 
-      <div className="p-2">
-        <h1 className="text-gray-600 text-sm lg:text-base line-clamp-1">
-          {title}
-        </h1>
-        <div className="flex items-center justify-between">
-          <p className="text-base lg:text-lg font-bold line-clamp-1">
-            {description}
-          </p>
-          <p className="block mt-2 text-black font-semibold lg:text-lg">
-            ${price}
-          </p>
+      {showProductDetails && (
+        <div className="p-2">
+          <h1 className="text-gray-600 text-sm lg:text-base line-clamp-1">
+            {name}
+          </h1>
+          <div className="flex items-center justify-between">
+            <p className="text-base lg:text-lg font-bold line-clamp-1">
+              {description}
+            </p>
+            <p className="block mt-2 text-black font-semibold lg:text-lg">
+              ${price}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </Link>
   );
 };
