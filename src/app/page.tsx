774176de77f980c38e03662bdbc69b2a05063fc8
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-02 23:42:37 +0000

    refactor: render landing page component on inital page component instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import NewProducts from "./homepage/components/NewProducts";
-import Hero from "./homepage/components/Hero";
+import LandingPage from "./(homepage)/page";
 import Layout from "./shared/components/Layout";
-import { getNewCollections, getNewThisWeek } from "@/services/products/products";
 
 export default async function Home() {
-  const newCollections = await getNewCollections();
-  const newThisWeek = await getNewThisWeek();
-
   return (
     <Layout showFooter={true}>
-      <Hero products={newCollections} />
-      <NewProducts products={newThisWeek} />
+      <LandingPage />
     </Layout>
   );
 }
