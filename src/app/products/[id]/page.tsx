f99afc335108b7d85da4dec809b3e0fec2bfc94c
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-03 23:36:06 +0000

    feat: get product details from backend

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,23 +1,31 @@
 "use client";
 
-import img1 from "../../../../public/images/product_1.png";
-import img2 from "../../../../public/images/product_2.png";
 import ProductImages from "./components/ProductImages";
 import ProductInfo from "./components/ProductInfo";
 import AddToCartButton from "./components/AddToCartButton";
 import Layout from "@/app/shared/components/Layout";
 import AddToFavoriteButton from "@/app/shared/components/AddToFavoriteButton";
-
-const thumbnails = [img1, img2];
+import useFetch from "@/hooks/useFetch";
+import { IProductDetails } from "@/modules/interfaces/products.interface";
 
 const ProductDetails = ({ params }: { params: { id: string } }) => {
+  const { data, loading, error } = useFetch<IProductDetails>(
+    `/product/${params.id}`
+  );
+
+  if (loading) return <div>Loading...</div>;
 
   return (
     <Layout showFooter={false}>
       <div className="container mx-auto lg:w-[80%] py-[5%]">
         <div className="flex flex-col lg:flex-row lg:justify-center lg:gap-[3%] items-start">
           <div className="flex flex-col lg:flex-row lg:w-2/4 w-full lg:p-6">
-            <ProductImages mainImage={img1} thumbnails={thumbnails} />
+            {data ? (
+              <ProductImages
+                mainImage={data?.images[0]}
+                thumbnails={data?.images}
+              />
+            ) : null}
           </div>
 
           <div className="lg:w-[30%] w-full lg:p-6 lg:pt-10 pt-10 py-6 px-4 mt-6 lg:mt-0 relative border-2 border-[#D9D9D9]">
@@ -25,7 +33,17 @@ const ProductDetails = ({ params }: { params: { id: string } }) => {
               <AddToFavoriteButton />
             </div>
 
-            <ProductInfo />
+            {data && (
+              <ProductInfo
+                key={data.id}
+                name={data.name}
+                price={data.price}
+                description={data.description}
+                colors={data.colors}
+                sizes={data.sizes}
+                type={data.type}
+              />
+            )}
 
             <AddToCartButton productId={params.id} />
           </div>
