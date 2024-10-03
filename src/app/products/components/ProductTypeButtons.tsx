fd980d7124c2637ac9b39d65685a1b1df5e7cb4b
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-03 21:04:15 +0000

    feat: create produt type buttons component for filtering products by type

---
+++ src/app/products/components/ProductTypeButtons.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { ProductTypeButton } from "@/modules/types/common.type";
+import { setSelectedType } from "@/redux/features/filtersSlice";
+import { useAppDispatch } from "@/redux/store";
+import { getButtonTypes } from "@/services/products/api";
+import { useEffect, useState } from "react";
+
+
+const ProductTypeButtons = () => {
+  const [productTypes, setProductTypes] = useState<ProductTypeButton[]>([]);
+
+  useEffect(() => {
+    const fetchButtonTypes = async () => {
+      try {
+        const types = await getButtonTypes();
+        setProductTypes(types);
+      } catch (error) {
+        console.error("Error fetching products button types:", error);
+      }
+    };
+
+    fetchButtonTypes();
+  }, []);
+
+  const dispatch = useAppDispatch();
+  const [selectedProductType, setSelectedProductType] = useState<string>("");
+
+  const handleProductTypeChange = (type: string) => {
+    setSelectedProductType(type);
+    dispatch(setSelectedType(type));
+  };
+
+  return (
+    <div className="w-full h-full grid grid-rows-2 grid-flow-col overflow-x-auto scrollbar-none gap-y-[2px] gap-x-4">
+      {productTypes.map((type) => (
+        <button
+          key={type.id}
+          type="button"
+          className={`flex justify-center items-center text-xs h-5 border border-[#D9D9D9] p-2.5 hover:bg-[#D9D9D9] ${
+            selectedProductType === type.name
+              ? "bg-black text-white"
+              : "bg-white text-black"
+          }`}
+          onClick={() => handleProductTypeChange(type.name)}
+        >
+          {type.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ProductTypeButtons;
