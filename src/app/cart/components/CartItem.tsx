be5ef08bfa9f93edb463ea140722f9eb32afc37d
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-04 15:36:17 +0000

    refactor: update cart item to handle item removal, increase and decrease item quantity

--- src/app/cart/components/CartItem.tsx
+++ src/app/cart/components/CartItem.tsx
@@ -3,74 +3,80 @@
 import Image from "next/image";
 import { FiX } from "react-icons/fi";
 import { ICartItem } from "@/modules/interfaces/products.interface";
-import AddToFavoriteButton from "@/app/shared/components/AddToFavoriteButton";
 import { FaMinus, FaPlus } from "react-icons/fa6";
 import Link from "next/link";
 
 const CartItem = ({
-  thumbnail,
-  name,
-  type,
-  price,
-  id,
-  quantity,
-  size,
-  color,
-}: ICartItem) => {
+  item,
+  removeItem,
+  increaseQuantity,
+  decreaseQuantity,
+}: {
+  item: ICartItem;
+  removeItem: (id: string) => void;
+  increaseQuantity: (id: string) => void;
+  decreaseQuantity: (id: string) => void;
+}) => {
   return (
     <div className="flex justify-between relative lg:w-[350px]">
       <button
         type="button"
         className="absolute top-0 right-0 text-gray-600 hover:text-gray-800"
+        onClick={() => removeItem(item.id)}
       >
         <FiX size={20} />
       </button>
       <Link
-        href={`/products/${id}`}
-        className="w-[300px] max-h-[350px] lg:max-h-[450px] bg-white overflow-hidden cursor-pointer"
+        href={`/products/${item.id}`}
+        className="w-[300px]  bg-white overflow-hidden cursor-pointer"
       >
         <div className="relative">
           <Image
-            src={thumbnail}
-            alt={name}
-            className="w-full max-h-[200px] lg:max-h-[400px] object-cover border-2 border-[#D9D9D9]"
+            src={item.images[0]}
+            alt={item.name}
+            className="w-full max-h-[250px] lg:max-h-[350px] object-contain border-2 border-[#D9D9D9]"
             width={300}
             height={376}
             priority
           />
           <div className="absolute bottom-0 right-0">
-            <AddToFavoriteButton />
+            {/* <AddToFavoriteButton /> */}
           </div>
         </div>
 
         <div className="p-2">
           <h1 className="text-gray-600 text-sm lg:text-base line-clamp-1 font-beatrice">
-            {type}
+            {item.type}
           </h1>
           <div className="flex items-center justify-between">
             <p className="text-base lg:text-lg font-medium line-clamp-1 font-beatrice">
-              {name}
+              {item.name}
             </p>
             <p className="block mt-2 text-black font-medium lg:text-lg">
-              ${price}
+              ${item.price}
             </p>
           </div>
         </div>
       </Link>
 
       <div className="flex flex-col justify-center gap-4">
-        <p className="text-sm font-bold text-black">{size}</p>
-
-        <div className="w-7 h-7" style={{ background: color }}></div>
+        <p className="text-sm font-bold text-black">{item.size}</p>
+        <div className="w-7 h-7" style={{ background: item.color }}></div>
 
         <div className="flex flex-col items-center">
-          <button className="w-7 h-7 flex text-xs items-center justify-center border border-[#8A8A8A] hover:bg-gray-200 disabled:text-[#8A8A8A]">
+          <button
+            className="w-7 h-7 flex text-xs items-center justify-center border border-[#8A8A8A] hover:bg-gray-200"
+            onClick={() => increaseQuantity(item.id)}
+          >
             <FaPlus />
           </button>
           <span className="w-7 h-7 flex text-sm font-bold items-center justify-center border border-[#8A8A8A]">
-            {quantity}
+            {item.quantity}
           </span>
-          <button className="w-7 h-7 flex text-xs items-center justify-center border border-[#8A8A8A] hover:bg-gray-200 disabled:text-[#8A8A8A]">
+          <button
+            className="w-7 h-7 flex text-xs items-center justify-center border border-[#8A8A8A] hover:bg-gray-200"
+            onClick={() => decreaseQuantity(item.id)}
+          >
             <FaMinus />
           </button>
         </div>
@@ -78,5 +84,4 @@ const CartItem = ({
     </div>
   );
 };
-
 export default CartItem;
