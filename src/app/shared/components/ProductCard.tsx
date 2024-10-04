60a75369e1ab37c7129af13f3e626f29142e51b8
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-04 10:15:11 +0000

    refactor: update product card image height

--- src/app/shared/components/ProductCard.tsx
+++ src/app/shared/components/ProductCard.tsx
@@ -23,7 +23,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
         <Image
           src={thumbnail}
           alt={name}
-          className="w-full max-h-[300px] lg:max-h-[350px] object-cover border-2 border-[#D9D9D9]"
+          className="w-full max-h-[250px] lg:max-h-[350px] object-cover border-2 border-[#D9D9D9]"
           width={300}
           height={376}
           priority
