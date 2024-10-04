c972beb91751580bb55b9894b356109b52fa66a7
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-04 15:29:34 +0000

    refactor: update add to cart button to handle a function as props

--- src/app/products/[id]/components/AddToCartButton.tsx
+++ src/app/products/[id]/components/AddToCartButton.tsx
@@ -1,15 +1,12 @@
-interface IAddToCartButtonProps {
-  productId: string;
+interface AddToCartButtonProps {
+  onAddToCart: () => void;
 }
 
-const AddToCartButton: React.FC<IAddToCartButtonProps> = ({ productId }) => {
-  const handleAddToCart = () => {
-    console.log(`Product ${productId} added to cart`);
-  };
-
+const AddToCartButton: React.FC<AddToCartButtonProps> = ({ onAddToCart }) => {
   return (
     <button
-      onClick={handleAddToCart}
+      type="button"
+      onClick={onAddToCart}
       className="hover:bg-black font-semibold hover:text-white bg-[#D9D9D9] text-black transition-all ease-in-out px-4 py-2 w-full"
     >
       ADD
