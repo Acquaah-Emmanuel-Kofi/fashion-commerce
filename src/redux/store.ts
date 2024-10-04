21baf6709fe93f550cd613ceeab7482b81b1df25
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-04 15:25:42 +0000

    feat: add cart slice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import productReducer from "@/redux/features/productSlice";
+import cartReducer from "@/redux/features/cartSlice";
 import searchReducer from "@/redux/features/searchSlice";
 import filtersReducer from "@/redux/features/filtersSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const rootReducer = combineReducers({
   products: productReducer,
+  cart: cartReducer,
   search: searchReducer,
   filters: filtersReducer,
 });
