0bc3ebc225f87be71fe54a50813c99825ba37d7e
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-07 09:35:28 +0000

    refactor: adjust images and info on product details page

--- src/app/products/[id]/components/ProductImages.tsx
+++ src/app/products/[id]/components/ProductImages.tsx
@@ -19,9 +19,9 @@ const ProductImages: React.FC<IProductImagesProps> = ({
   };
 
   return (
-    <div className="flex flex-col lg:flex-row lg:w-full max-h-[500px]">
+    <div className="flex flex-col lg:flex-row lg:w-full gap-5 min-h-[500px] max-h-[500px] lg:max-h-[700px]  border-2 border-yellow-500">
       {/* Main image */}
-      <div className="w-full lg:w-3/4 mb-4">
+      <div className="w-full mb-4">
         <Image
           src={selectedImage}
           alt="Product Image"
@@ -29,17 +29,17 @@ const ProductImages: React.FC<IProductImagesProps> = ({
           width={900}
           height={200}
           priority
-          className="w-full object-contain max-h-[200px] lg:max-h-[450px]"
+          className="w-full h-full object-cover max-w-[450px] border-2 border-green-500"
         />
       </div>
 
       {/* Thumbnails */}
-      <div className="w-full lg:w-1/4 lg:flex lg:flex-col lg:items-center flex space-x-3 overflow-auto scrollbar-none">
+      <div className="w-full lg:w-1/6 lg:flex lg:flex-col lg:items-center flex space-x-3 lg:space-x-0 overflow-auto scrollbar-none border-2 border-red-500">
         <div className="flex lg:flex-col gap-2">
           {thumbnails?.map((thumb, idx) => (
             <div
               key={idx}
-              className={`w-14 h-16 cursor-pointer ${
+              className={`cursor-pointer ${
                 selectedImage === thumb ? "opacity-100" : "opacity-50"
               } hover:opacity-100 transition-opacity duration-300`}
               onClick={() => handleThumbnailClick(thumb)}
