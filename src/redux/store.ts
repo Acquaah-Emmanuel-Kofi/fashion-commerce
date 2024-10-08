a73e04b2df2a2d07c89fd7ca0a42959d1328853d
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-08 21:09:51 +0000

    feat: add favoriteSlice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,11 +4,13 @@ import cartReducer from "@/redux/features/cartSlice";
 import searchReducer from "@/redux/features/searchSlice";
 import filtersReducer from "@/redux/features/filtersSlice";
 import collectionSlice from "@/redux/features/collectionSlice";
+import favoriteSlice from "@/redux/features/favoriteSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const rootReducer = combineReducers({
   products: productReducer,
   cart: cartReducer,
+  favorites: favoriteSlice,
   search: searchReducer,
   filters: filtersReducer,
   collections: collectionSlice,
