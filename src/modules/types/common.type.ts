26bc656f9e02d04ba1deaa053aecd7552d6ac3c8
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-08 14:49:09 +0000

    feat: create a type for form fields

--- src/modules/types/common.type.ts
+++ src/modules/types/common.type.ts
@@ -2,3 +2,15 @@ export type ProductTypeButton = {
   id: string;
   name: string;
 };
+
+export type FormFields = {
+  email: string;
+  phone: string;
+  firstname: string;
+  lastname: string;
+  country: string;
+  state: string;
+  address: string;
+  city: string;
+  postalCode: string;
+};
