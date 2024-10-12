7c5f87775826ff644fd0c6e7b897a15b084978bd
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-12 12:23:55 +0000

    feat: implement form validation and error handling feedback for users

--- src/app/admin/(pages)/products/components/ProductForm.tsx
+++ src/app/admin/(pages)/products/components/ProductForm.tsx
@@ -1,7 +1,8 @@
 import InputField from "@/app/shared/components/InputField";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MultiImageUpload from "./MultiImageUpload";
 import Image from "next/image";
+import toast from "react-hot-toast";
 
 interface ProductFormProps {
   product?: {
@@ -16,7 +17,6 @@ interface ProductFormProps {
     tags: string[];
     images: File[];
   };
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onSubmit: (data: any) => void;
   onDelete?: () => void;
 }
@@ -38,7 +38,13 @@ const ProductForm: React.FC<ProductFormProps> = ({
     tags: product?.tags || ["Name", "Test", "Again"],
     images: product?.images || [],
   });
+
   const [productImage, setProductImage] = useState<string>("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    validateForm();
+  }, [formData]);
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -63,8 +69,53 @@ const ProductForm: React.FC<ProductFormProps> = ({
     }));
   };
 
+  const validateForm = () => {
+    const {
+      name,
+      description,
+      category,
+      brand,
+      stock,
+      regularPrice,
+      salePrice,
+      images,
+    } = formData;
+
+    const isValid =
+      !!name.trim() &&
+      !!description.trim() &&
+      !!category.trim() &&
+      !!brand.trim() &&
+      stock > 0 &&
+      regularPrice > 0 &&
+      salePrice >= 0 &&
+      images.length > 0;
+
+    setIsFormValid(isValid);
+  };
+
   const handleSubmit = () => {
-    onSubmit(formData);
+    if (isFormValid) {
+      onSubmit(formData);
+    } else {
+      const missingFields = [];
+
+      if (!formData.name.trim()) missingFields.push("Product Name");
+      if (!formData.description.trim()) missingFields.push("Description");
+      if (!formData.category.trim()) missingFields.push("Category");
+      if (!formData.brand.trim()) missingFields.push("Brand");
+      if (formData.stock <= 0) missingFields.push("Stock Quantity");
+      if (formData.regularPrice <= 0) missingFields.push("Regular Price");
+      if (formData.salePrice < 0) missingFields.push("Sale Price");
+      if (formData.images.length === 0)
+        missingFields.push("Add at least one image");
+
+      const message = `Please fill all Product Details: \n ${missingFields.join(
+        ", "
+      )}`;
+
+      toast.error(message);
+    }
   };
 
   const handleCancel = () => {
@@ -87,92 +138,77 @@ const ProductForm: React.FC<ProductFormProps> = ({
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-6">
         {/* Left Side Form Fields */}
         <div className="space-y-4">
-          <div>
-            <InputField
-              name="name"
-              placeholder="Type name here"
-              label="Product Name"
-              value={formData.name}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <InputField
-              name="description"
-              placeholder="Type description here"
-              label="Description"
-              value={formData.description}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <InputField
-              name="category"
-              placeholder="Type category here"
-              label="Category"
-              value={formData.category}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <InputField
-              name="brand"
-              placeholder="Type brand name here"
-              label="Brand Name"
-              value={formData.brand}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
+          <InputField
+            name="name"
+            placeholder="Type name here"
+            label="Product Name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+          <InputField
+            name="description"
+            placeholder="Type description here"
+            label="Description"
+            value={formData.description}
+            onChange={handleInputChange}
+            required
+          />
+          <InputField
+            name="category"
+            placeholder="Type category here"
+            label="Category"
+            value={formData.category}
+            onChange={handleInputChange}
+            required
+          />
+          <InputField
+            name="brand"
+            placeholder="Type brand name here"
+            label="Brand Name"
+            value={formData.brand}
+            onChange={handleInputChange}
+            required
+          />
           <div className="grid lg:grid-cols-2 grid-cols-1 gap-4">
-            <div>
-              <InputField
-                name="sku"
-                placeholder="SKU"
-                label="SKU"
-                value={formData.sku}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <InputField
-                name="stock"
-                placeholder="Stock"
-                label="Stock Quantity"
-                type="number"
-                value={String(formData.stock)}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
+            <InputField
+              name="sku"
+              placeholder="SKU"
+              label="SKU"
+              value={formData.sku}
+              onChange={handleInputChange}
+            />
+            <InputField
+              name="stock"
+              placeholder="Stock"
+              label="Stock Quantity"
+              type="number"
+              value={String(formData.stock)}
+              onChange={handleInputChange}
+              required
+            />
           </div>
           <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-4">
-            <div>
-              <InputField
-                name="regularPrice"
-                placeholder="Price"
-                label="Regular Price"
-                type="number"
-                value={String(formData.regularPrice)}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-            <div>
-              <InputField
-                name="salePrice"
-                placeholder="PriSalece"
-                label="Sale Price"
-                type="number"
-                value={String(formData.salePrice)}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
+            <InputField
+              name="regularPrice"
+              placeholder="Price"
+              label="Regular Price"
+              type="number"
+              value={String(formData.regularPrice)}
+              onChange={handleInputChange}
+              required
+            />
+            <InputField
+              name="salePrice"
+              placeholder="Sale Price"
+              label="Sale Price"
+              type="number"
+              value={String(formData.salePrice)}
+              onChange={handleInputChange}
+              required
+            />
           </div>
+
           {/* Tags */}
           <div className="mt-4">
             <label className="block font-medium mb-1">Tags</label>
@@ -198,7 +234,6 @@ const ProductForm: React.FC<ProductFormProps> = ({
 
         {/* Right Side Image Upload and Gallery */}
         <div className="space-y-6">
-          {/* <ImageUpload onImageSelect={handleImageSelect} /> */}
           <div className="w-full h-[350px] bg-gray-200">
             {formData.images.length === 0 ? (
               <div className="w-full h-full flex justify-center items-center">
@@ -220,7 +255,6 @@ const ProductForm: React.FC<ProductFormProps> = ({
       </div>
 
       <div className="flex justify-end w-full mt-6">
-        {/* Form Action Buttons */}
         <div className="grid grid-cols-2 lg:w-3/12 w-full gap-3">
           {onDelete && (
             <button
@@ -234,8 +268,9 @@ const ProductForm: React.FC<ProductFormProps> = ({
           <button
             type="button"
             onClick={handleSubmit}
-            disabled={true}
-            className="w-full py-2 bg-black text-white disabled:bg-gray-600"
+            className={`w-full py-2 text-white ${
+              isFormValid ? "bg-black" : "bg-gray-600"
+            }`}
           >
             {product ? "UPDATE" : "SUBMIT"}
           </button>
