e8272156d6c692385c13ca4cb78658a5ea03c6d5
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-12 10:44:10 +0000

    feat: route user to add new product page when button is clicked on products page

--- src/app/admin/(pages)/products/page.tsx
+++ src/app/admin/(pages)/products/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Breadcrumb from "@/app/shared/components/Breadcrumb";
 import React from "react";
 import HeaderTitle from "../../(components)/HeaderTitle";
 import { LuPlusCircle } from "react-icons/lu";
 import ProductList from "./components/ProductList";
 import SearchBar from "@/app/shared/components/Searchbar";
+import { useRouter } from "next/navigation";
 
 const breadcrumbItems = [
   { label: "Home", href: "/admin" },
@@ -11,6 +14,10 @@ const breadcrumbItems = [
 ];
 
 export default function AllProducts() {
+  const router = useRouter();
+
+  const handleAddNewProductRoute = () => router.push("/admin/products/add-new-product");
+  
   return (
     <section className="space-y-6 pb-6">
       <header>
@@ -22,6 +29,7 @@ export default function AllProducts() {
           <div className="lg:w-[200px] w-full">
             <button
               type="button"
+              onClick={handleAddNewProductRoute}
               className="hover:bg-gray-500 bg-black text-sm flex items-center gap-2 justify-center font-semibold hover:text-black text-white transition-all ease-in-out p-2 w-full"
             >
               <LuPlusCircle />
