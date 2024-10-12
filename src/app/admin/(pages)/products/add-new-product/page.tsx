2436fa395a208479e1d4276994e5417576898c6b
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-12 10:46:38 +0000

    feat: structure page with product form

--- src/app/admin/(pages)/products/add-new-product/page.tsx
+++ src/app/admin/(pages)/products/add-new-product/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import ProductForm from "../components/ProductForm";
+import HeaderTitle from "@/app/admin/(components)/HeaderTitle";
 
 export default function AddNewProduct() {
-    return (
-        <div>Add new product</div>
-    )
+  const handleSubmit = (data: any) => {
+    console.log("Product Data:", data);
+  };
+  return (
+    <section className="space-y-6 pb-6">
+      <HeaderTitle title="Add Product" />
+      <ProductForm onSubmit={handleSubmit} />
+    </section>
+  );
 }
