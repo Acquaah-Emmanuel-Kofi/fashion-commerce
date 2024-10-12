fc10c43d2b085ff6c9a2d70b5171e169e3ce99b6
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-12 10:40:50 +0000

    refactor: update inputfield label style

--- src/app/shared/components/InputField.tsx
+++ src/app/shared/components/InputField.tsx
@@ -30,7 +30,7 @@ const InputField: React.FC<InputFieldProps> = ({
       {label && (
         <label
           htmlFor={name}
-          className="block text-sm font-medium text-gray-700"
+          className="block font-medium text-black mb-1"
         >
           {label} {required && <span className="text-red-500">*</span>}
         </label>
