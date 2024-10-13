f3d71bde073578a81794f984cf5e506dbf77e233
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-13 22:38:45 +0000

    refactor: add font family beatrice to order summary texts

--- src/app/admin/(pages)/orders/[orderId]/components/OrderTotalSummary.tsx
+++ src/app/admin/(pages)/orders/[orderId]/components/OrderTotalSummary.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 
 export default function OrderTotalSummary () {
     return (
-      <div className="p-4 bg-white space-y-4">
+      <div className="p-4 bg-white space-y-4 lg:w-[400px] w-full">
         <h2 className="font-semibold font-beatrice">Order Total Summary</h2>
         <div className="flex justify-between w-full ">
-          <p className="text-base">Subtotal</p>
+          <p className="text-base font-beatrice">Subtotal</p>
           <p className="font-semibold text-base">GHS3,203</p>
         </div>
 
         <div className="flex justify-between w-full ">
-          <p className="text-base">Tax (20%)</p>
+          <p className="text-base font-beatrice">Tax (20%)</p>
           <p className="font-semibold text-base">GHS6,023</p>
         </div>
 
         <div className="flex justify-between w-full ">
-          <p className="text-base">Discount</p>
+          <p className="text-base font-beatrice">Discount</p>
           <p className="font-semibold text-base">GHS0</p>
         </div>
 
-        <div className='border border-dashed border-gray-200'></div>
+        <div className="border border-dashed border-gray-200"></div>
 
         <div className="flex justify-between w-full ">
-          <p className="text-xl font-semibold">Total</p>
+          <p className="text-xl font-semibold font-beatrice">Total</p>
           <p className="font-semibold text-xl">GHS9,226</p>
         </div>
       </div>
