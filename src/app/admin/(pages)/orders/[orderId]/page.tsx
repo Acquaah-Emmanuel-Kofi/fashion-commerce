febc30aa3fb745b25a2958ab5442013bf7d8b967
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-13 22:12:38 +0000

    feat: structure page with components

---
+++ src/app/admin/(pages)/orders/[orderId]/page.tsx
@@ -0,0 +1,56 @@
+import HeaderTitle from "@/app/admin/(components)/HeaderTitle";
+import Breadcrumb from "@/app/shared/components/Breadcrumb";
+import React from "react";
+import OrderDetailsInfo from "./components/OrderDetailsInfo";
+
+const breadcrumbItems = [
+  { label: "Order List", href: "/admin/orders" },
+  { label: "Order Details", href: "" },
+];
+
+export default function OrderDetails({
+  params,
+}: {
+  params: { orderId: string };
+}) {
+  const orderDetails = {
+    orderId: "6743",
+    customer: {
+      name: "Shristi Singh",
+      email: "shristi@gmail.com",
+      phone: "091 904 231 1212",
+    },
+    orderInfo: {
+      shippingMethod: "Next Express",
+      paymentMethod: "Paypal",
+      status: "Pending",
+    },
+    deliveryAddress: {
+      address: "Dharam Colony",
+      city: "Gurgaon",
+      region: "Haryana",
+      country: "India",
+    },
+    paymentInfo: {
+      cardType: "MasterCard",
+      maskedCardNumber: "**** **** **** 6557",
+      businessName: "Shristi Singh",
+      phone: "091 904 231 1212",
+    },
+    orderDate: {
+      start: "Feb 16, 2022",
+      end: "Feb 20, 2022",
+    },
+  };
+
+  return (
+    <section className="space-y-6 pb-6">
+      <header>
+        <HeaderTitle title="Order Details" />
+        <Breadcrumb items={breadcrumbItems} />
+      </header>
+
+      <OrderDetailsInfo {...orderDetails} />
+    </section>
+  );
+}
