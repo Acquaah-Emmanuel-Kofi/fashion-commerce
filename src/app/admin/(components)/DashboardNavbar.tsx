9cbfb44fc34ee94efb2ae1526eb6c8f7d4412dbf
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-14 14:38:58 +0000

    feat: add menu button to dashboard navbar to toggle sidebar

--- src/app/admin/(components)/DashboardNavbar.tsx
+++ src/app/admin/(components)/DashboardNavbar.tsx
@@ -9,6 +9,38 @@ export default function DashboardNavbar({
 }) {
   return (
     <div className="flex lg:justify-end justify-between items-center p-4 bg-white border-b z-50">
+      <button
+        type="button"
+        className="block lg:hidden mr-4 text-gray-600"
+        onClick={toggleSidebar}
+      >
+        <svg
+          width="28"
+          height="18"
+          viewBox="0 0 28 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M27 1L1 1"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+          <path
+            d="M19 9L1 9"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+          <path
+            d="M14 17H1"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+        </svg>
+      </button>
 
       <div className="flex items-center space-x-4">
         <FaBell className="text-gray-500" />
