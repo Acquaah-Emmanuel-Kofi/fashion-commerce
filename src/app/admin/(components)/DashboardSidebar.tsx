0dc5c9d3f6f0d0daf69af305dd15663ff168f8f5
waglarry <emmanuelacquaah2468@gmail.com>
2024-10-14 14:40:07 +0000

    feat: add close button to toggle sidebar

--- src/app/admin/(components)/DashboardSidebar.tsx
+++ src/app/admin/(components)/DashboardSidebar.tsx
@@ -27,6 +27,27 @@ export default function DashboardSidebar({
         <div className="flex items-center space-x-2 p-4">
           <Brand />
         </div>
+
+        <button
+          type="button"
+          onClick={toggleSidebar}
+          className="md:hidden block text-gray-600"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        </button>
       </div>
 
       <nav className="mt-6">
