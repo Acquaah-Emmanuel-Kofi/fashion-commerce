cdc2e28e14b4f55ca649f8ad3a4e5d5fb54ac5ad
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-10-17 09:06:55 +0000

    refactor: reduce revalidate instead

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ export const fetchDataFromApi = async (endpoint: string) => {
       "https://fashion-commerce.onrender.com/api/v1";
     const url = `${baseUrl}${endpoint}`;
 
-    const response = await fetch(url, { cache: 'no-store' });
+    const response = await fetch(url, { next: { revalidate: 10 } });
 
     if (!response.ok) {
       throw new Error("Failed to fetch data");
