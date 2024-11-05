3f01c35a34324e1cd6df518b83640875f854596d
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-05 08:31:39 +0000

    feat: create IStats interface from backend

--- src/modules/interfaces/analytics.interface.ts
+++ src/modules/interfaces/analytics.interface.ts
@@ -2,3 +2,10 @@ export interface IAnalytics {
     labels: string[];
     data: number[]
 }
+
+export interface IStats {
+  cancelledOrders: number;
+  deliveredOrders: number;
+  pendingOrders: number;
+  totalOrder: number;
+}
