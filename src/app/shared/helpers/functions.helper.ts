a42ff82ce3720b9238750aa3d1663541619c88a1
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-05 09:10:03 +0000

    feat: create a function to get previous and current date

--- src/app/shared/helpers/functions.helper.ts
+++ src/app/shared/helpers/functions.helper.ts
@@ -10,3 +10,17 @@ export const formatDate = (dateString: string): string => {
 export const padWithZero = (number: number) => {
   return number.toString().padStart(2, "0");
 }
+
+export const getPreviousAndCurrentDate = (): string => {
+  const currentDate: Date = new Date();
+  const previousDate: Date = new Date();
+  previousDate.setMonth(currentDate.getMonth() - 1); 
+
+  const options: Intl.DateTimeFormatOptions = { month: "short", day: "2-digit", year: "numeric" };
+  const dateFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("en-US", options);
+
+  const formattedCurrentDate: string = dateFormatter.format(currentDate);
+  const formattedPreviousDate: string = dateFormatter.format(previousDate);
+
+  return `${formattedPreviousDate} - ${formattedCurrentDate}`;
+}
